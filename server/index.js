86719a5f4d1b6b3fb6bb0668dc5f9c2ee6693615
SmelayaPanda <SmelayaPanda@mail.ru>
2018-10-05 01:22:55 +0700

    text

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({hi: 'Ree'});
+    res.send({hi: 'Reex'});
 })
 
 // because heroku server can run many app one one machine,
