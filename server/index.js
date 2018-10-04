985391ad250273af15555754ee9a561b563b4c8c
SmelayaPanda <SmelayaPanda@mail.ru>
2018-10-05 00:35:10 +0700

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+// inside one node app can be several app instances
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send({hi: 'There'});
+})
+
+// because heroku server can run many app one one machine,
+// we use dynamic port binding form env variables
+const PORT = process.env.PORT || 5000; // => for dev will be used 5000 port
+app.listen(PORT)
