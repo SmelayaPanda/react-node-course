ff6101b629ef1a439e1df0af60ad0de999449292
SmelayaPanda <SmelayaPanda@mail.ru>
2018-11-07 15:16:43 +0700

    user schema

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose')
 const keys = require('./config/keys')
+require('./models/User') // before passport because it used model
 require('./services/passport')
 
 mongoose.connect(keys.mongoURI)
--- server/services/passport.js
+++ server/services/passport.js
@@ -1,7 +1,9 @@
 const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose')
 const keys = require('../config/keys')
 
+const User = mongoose.model('users')
 // https://console.developers.google.com
 passport.use(
     new GoogleStrategy({
@@ -10,9 +12,14 @@ passport.use(
             callbackURL: '/auth/google/callback'
         },
         (accessToken, refreshToken, profile, done) => {
-            console.log('accessToken', accessToken);
-            console.log('refreshToken', refreshToken);
-            console.log('profile', profile);
-            // console.log('done', done);
+            User.findOne({googleId: profile.id}).then(existingUser => {
+                console.log(existingUser);
+                if (existingUser) {
+                    // ...
+                } else {
+                    new User({googleId: profile.id}).save()
+                }
+            })
+
         })
 );
