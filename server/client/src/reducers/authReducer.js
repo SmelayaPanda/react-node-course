fcfda1fdcc1e402d8f6a95036cbd4beec2a4b035
SmelayaPanda <SmelayaPanda@mail.ru>
2018-11-20 15:53:26 +0700

    three auth states

--- server/client/src/reducers/authReducer.js
+++ server/client/src/reducers/authReducer.js
@@ -1,7 +1,15 @@
-export default function (state = {}, action) {
-    console.log(action);
+import {FETCH_USER} from  '../actions/types'
+
+export default function (state = null, action) {
     switch (action.type) {
+        case FETCH_USER:
+            return action.payload || false
         default:
             return state
     }
 }
+
+// Auth reducer returns three states:
+// null - request pending (we really don't know exists user or not now)
+// false - not exists
+// object - user exists (contains existing user id)
