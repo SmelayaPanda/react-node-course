a128925f70299c3265e07be7c070c233e22ea665
SmelayaPanda <SmelayaPanda@mail.ru>
2018-11-20 15:24:40 +0700

    apply redux thunk middleware

--- server/client/src/setupProxy.js
+++ server/client/src/setupProxy.js
@@ -4,6 +4,7 @@
 
 const proxy = require('http-proxy-middleware')
 
-module.exports = function(app) {
-    app.use(proxy('/auth/google', { target: 'http://localhost:5000' }))
+module.exports = function (app) {
+    app.use(proxy('/auth/google', {target: 'http://[::1]:5000'}))
+    app.use(proxy('/api/*', {target: 'http://[::1]:5000'}))
 }
