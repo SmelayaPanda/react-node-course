ca565fff7a976b031d1eb220156241bfe223aaec
SmelayaPanda <SmelayaPanda@mail.ru>
2018-11-22 20:24:52 +0700

    improve survey schema

--- models/Survey.js
+++ models/Survey.js
@@ -1,11 +1,19 @@
 const mongoose = require('mongoose')
 const {Schema} = mongoose
+const RecipientSchema = require('./Recipients')
 
 const surveySchema = new Schema({
     title: String,
     body: String,
     subject: String,
-    recipients: [String]
+    recipients: [RecipientSchema],
+    yes: {type: Number, default: 0},
+    no: {type: Number, default: 0},
+    dateSent: Date,
+    lastResponded: Date,
+    // Underscore is not required (and you can choice any name what you are want)
+    // But by convention we use this name as ref + undescore
+    _user: {type: Schema.Types.ObjectId, ref: 'User'}
 })
 
 mongoose.model('surveys', surveySchema)
