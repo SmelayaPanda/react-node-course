239b188e7bd1f5c18ab0fcab2c8c20372cc3a531
SmelayaPanda <SmelayaPanda@mail.ru>
2018-11-22 14:13:41 +0700

    heroku production build config

--- server/index.js
+++ server/index.js
@@ -25,6 +25,27 @@ app.use(passport.session())
 require('./routes/auth')(app)
 require('./routes/billing')(app)
 
+if (process.env.NODE_ENV === 'production') {
+    /*
+        Express will serve up the production assets like our main.js / main.css files.
+        If any get request comes in for some routes or some file or absolutely
+        anything to our application and we do not understand what it's looking for -> use assets directory
+    */
+    app.use(express.static('client/build')) // !!!
+
+    /*
+        Express will serve up index.html file if it doesn't recognise the route.
+        So when a request comes into express express we'll first check to see if there is some specific file
+        that matches up with what that request is looking for.
+        If there is it's going to answer the request with this line right here if there's not express will
+        then continue on down and we'll find this next route handler which we can essentially think of
+        as the absolute catchall inside of our application.
+    */
+    const path = require('path')
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 // because heroku server can run many app one one machine,
 // we use dynamic port binding form env variables
