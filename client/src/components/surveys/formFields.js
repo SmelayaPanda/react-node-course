faf5dcc9e2d7e6f672f6dd69ef02ca61c80fc1ae
SmelayaPanda <SmelayaPanda@mail.ru>
2018-11-24 23:14:23 +0700

    rename

--- client/src/components/surveys/formFields.js
+++ client/src/components/surveys/formFields.js
@@ -2,5 +2,5 @@ export default [
     {name: 'title', label: 'Survey Title'},
     {name: 'subject', label: 'Subject line'},
     {name: 'body', label: 'Email Body'},
-    {name: 'emails', label: 'Recipient List'}
+    {name: 'recipients', label: 'Recipient List'}
 ]
