070388dfb02a77be7e1316dd9b11c89999455b9b
SmelayaPanda <SmelayaPanda@mail.ru>
2018-11-25 14:13:16 +0700

    survey webhook

--- routes/survey.js
+++ routes/survey.js
@@ -1,3 +1,6 @@
+const _ = require('lodash')
+const {Path} = require('path-parser').default
+const {URL} = require('url')
 const mongoose = require('mongoose')
 const requireLogin = require('../middlewares/requireLogin')
 const requireCredits = require('../middlewares/requireCredits')
@@ -8,19 +11,42 @@ const Survey = mongoose.model('surveys')
 
 module.exports = app => {
     app.get(
-        '/api/surveys/thanks',
+        '/api/surveys/:surveyId/:choice',
         (req, res) => {
-            res.send('Thanks for voting')
-        }
-    )
+            res.send('Thanks for voting!')
+        })
 
-    app.get(
-        '/api/surveys/webhooks',
-        (req, res) => {
-            console.log(req.body);
-            res.send({})
-        }
-    )
+    app.post('/api/surveys/webhooks', (req, res) => {
+        const p = new Path('/api/surveys/:surveyId/:choice')
+
+        _.chain(req.body)
+            .map(({email, url}) => {
+                const match = p.test(new URL(url).pathname)
+                if (match) {
+                    return {email, surveyId: match.surveyId, choice: match.choice}
+                }
+            })
+            .compact()
+            .uniqBy('email', 'surveyId')
+            .each(({surveyId, email, choice}) => {
+                Survey.updateOne(
+                    {
+                        _id: surveyId,
+                        recipients: {
+                            $elemMatch: {email: email, responded: false}
+                        }
+                    },
+                    {
+                        $inc: {[choice]: 1},
+                        $set: {'recipients.$.responded': true},
+                        lastResponded: new Date()
+                    }
+                ).exec()
+            })
+            .value()
+
+        res.send({})
+    })
 
     app.post(
         '/api/surveys',
@@ -39,7 +65,9 @@ module.exports = app => {
                 dateSent: Date.now()
             })
 
+            // Great place to send an email!
             const mailer = new Mailer(survey, surveyTemplate(survey))
+
             try {
                 await mailer.send()
                 await survey.save()
